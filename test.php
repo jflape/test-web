2928a7603202b04bb5b245983e37d0619c6a48ba
jflape <jflape@davao-water.gov.ph>
2025-06-18 16:30:53 +0800

    Updated New Files

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
 // Display a welcome message
-echo "<h1>Welcome to My Test1 PHP Page</h1>";
+echo "<h1>Welcome to My Test PHP Page</h1>";
 
 // Display the current date and time
 echo "<p>Current Date and Time: " . date("Y-m-d H:i:s") . "</p>";
 
-echo "<h1>Welcome to My Test2 PHP Page</h1>";
-echo "<h1>Welcome to My Test3 PHP Page</h1>";
+echo "<h1>Welcome to My Test PHP Page</h1>";
+echo "<h1>Welcome to My Test PHP Page</h1>";
 // Display server information
 echo "<h2>PHP Configuration Info</h2>";
 phpinfo();
